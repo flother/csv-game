128378b02557ed2dd65284bc14e9d7aad707bf7d
Andy Georges <itkovian@gmail.com>
2014-09-10 15:10:00 +0200

    Count the fields using length instead of pre-defined info

--- haskell/Csv.hs
+++ haskell/Csv.hs
@@ -3,6 +3,7 @@
 import Control.Monad
 import qualified Data.ByteString as B
 import Data.Csv.Incremental
+import Data.Text
 import System.Exit
 import System.IO
 
@@ -10,14 +11,17 @@ import System.IO
 main :: IO ()
 main = withFile "/dev/stdin" ReadMode $ \ csvFile -> do
     let loop !_ (Fail _ errMsg) = putStrLn errMsg >> exitFailure
-        loop acc (Many rs k) = loop (acc + sumFields rs) =<< feed k
-        loop acc (Done rs) = putStrLn $ show (sumFields rs + acc)
+        loop !acc (Many rs k) = loop (acc + countFields rs) =<< feed k
+        loop !acc (Done rs) = putStrLn $ show (countFields rs + acc)
 
         feed k = do
             isEof <- hIsEOF csvFile
             if isEof
                 then return $ k B.empty
                 else k `fmap` B.hGetSome csvFile 4096
-    loop 0 (decode NoHeader)
+    loop 0 (decode NoHeader :: Parser [Data.Text.Text])
   where
-    sumFields rs = sum [5| Right (_ :: String, _ :: String, _ :: String, _ :: String, _ :: String) <- rs]
+    countFields :: [Either String [a]] -> Int
+    countFields rs = sum $ Prelude.map (\fs -> case fs of 
+                                                Right fs' -> Prelude.length fs'
+                                                Left _    -> 0) rs
