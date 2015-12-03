a8a7b8cf25400f1b266ec9849c8b9429a1166f6d
Johann Tuffe <tafia973@gmail.com>
2015-12-03 01:22:43 -0500

    update with last quick-csv changes

--- rust/quick-count/src/quick-count.rs
+++ rust/quick-count/src/quick-count.rs
@@ -5,13 +5,11 @@ fn main() {
     let field_num = args[1].parse::<usize>().unwrap() - 1;
     let filename = &args[2];
     let rdr = csv::Csv::from_file(filename).unwrap();
-    let mut sum = 0;
 
-    for row in rdr.into_iter() {
-        sum += row.columns()
-		  .nth(field_num + 1).unwrap()
-		  .parse::<i64>().unwrap();
-    }
+    let sum = rdr.into_iter()
+                 .map(|r| r.unwrap().columns().unwrap()
+                           .nth(field_num).unwrap().parse::<i64>().unwrap())
+                 .fold(0i64, |c, n| c + n);
 
     println!("{}", sum);
 }
