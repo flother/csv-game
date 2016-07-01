0f516bc61d7525d928f35690b66eab59faf07d78
Ewan Higgs <ewan_higgs@yahoo.co.uk>
2016-07-02 00:08:56 +0200

    Add pandas version as written by Kenneth Hoste.

--- python2/build.sh
+++ python2/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-TIMEFORMAT='python2,csv,%R'
-for i in $(seq 1 10); do 
-    (time ./csvreader.py < /tmp/hello.csv) 2>> ../results.csv
+for SCRIPT in csvreader.py csvreader-pandas.py ; do 
+  TIMEFORMAT="python2,$SCRIPT,%R"
+  for i in $(seq 1 10); do 
+      (time ./"$SCRIPT" < /tmp/hello.csv) 2>> ../results.csv
+  done
 done
---
+++ python2/csvreader-pandas.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''Read CSV file into pandas dataframe and print the number of fields'''
+
+import pandas as p
+import sys
+
+df = p.read_csv(sys.stdin, header=None)
+print df.shape[0] * df.shape[1]
