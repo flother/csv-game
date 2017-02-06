ebd9de584ec3f9283a228725ae1588160c8b7542
Ewan Higgs <ewan.higgs@hgst.com>
2017-02-06 15:56:23 +0100

    Added empty.csv job for awk regexp

--- awk/build.sh
+++ awk/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ../build.sh
 timer ../results.csv gawk "regexp" fieldcount "gawk -f ./fieldcount.awk /tmp/hello.csv"
+timer ../results.csv gawk "regexp" empty "gawk -f ./fieldcount.awk /tmp/empty.csv"
