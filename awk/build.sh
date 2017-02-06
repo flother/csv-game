2546af3f18003381e88e7cfcadc868e533c5328e
Ewan Higgs <ewan.higgs@hgst.com>
2017-02-06 14:42:30 +0100

    Run gawk as well as mawk.

--- awk/build.sh
+++ awk/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ../build.sh
-timer ../results.csv awk "regexp" fieldcount "./fieldcount.awk /tmp/hello.csv"
+timer ../results.csv gawk "regexp" fieldcount "./fieldcount.awk /tmp/hello.csv"
+timer ../results.csv mawk "regexp" fieldcount "./fieldcount.awk /tmp/hello.csv"
